608112212b17659dcc6ad69f3d40e450222e999d
dank00 <dk@amp.co>
2017-10-11 23:52:43 +0100

    Update InvalidCodeException.php

--- src/InvalidCodeException.php
+++ src/InvalidCodeException.php
@@ -4,17 +4,15 @@ namespace PHPostcode;
 
 class InvalidCodeException extends \Exception
 {
-    public function __construct(
-        Code $failingCode,
-        $code = 0,
-        \Throwable $previous = null
-    ) {
+    public function __construct(Code $failingCode)
+    {
         $message = sprintf(
             '%s format:(%s) does not match a valid format (%s)',
             $failingCode->toString(),
             $failingCode->getFormat(),
             implode(',', $failingCode->getValidFormats())
         );
-        parent::__construct($message, $code, $previous);
+        
+        parent::__construct($message);
     }
 }
